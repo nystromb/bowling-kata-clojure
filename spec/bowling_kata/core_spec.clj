25512de775232329e25697c485e5cbd03f8ea374
nystromb <bnystrom@siu.edu>
2015-12-30 14:27:40 -0600

    refactor and calculates spare bonus

--- spec/bowling_kata/core_spec.clj
+++ spec/bowling_kata/core_spec.clj
@@ -2,9 +2,13 @@
   (:require [speclj.core :refer :all]
             [bowling-kata.core :refer :all]))
 
-(defn roll-all [pins])
-
 (describe "a bowling game"
   (it "score is 0 if no pins are knocked down"
-		(should= 0 (score-game {1 [0 0] 2 [0 0] 3 [0 0] 4 [0 0] 5 [0 0] 6 [0 0] 7 [0 0] 8 [0 0], 9 [0 0] 10 [0 0] }))))
-
+    (should= 0 (score-game { 1 [0 0] 2 [0 0] 3 [0 0] 4 [0 0] 5 [0 0] 6 [0 0] 7 [0 0] 8 [0 0] 9 [0 0] 10 [0 0] })))
+	
+  (it "score is 20 if no pins are knocked down"
+    (should= 20 (score-game { 1 [1 1] 2 [1 1] 3 [1 1] 4 [1 1] 5 [1 1] 6 [1 1] 7 [1 1] 8 [1 1] 9 [1 1] 10 [1 1] })))
+	
+  (it "adds extra bonus for spares"
+    (should= 20 (score-game { 1 [5 5] 2 [3 3] 3 [1 0] 4 [0 0] 5 [0 0] 6 [0 0] 7 [0 0] 8 [0 0] 9 [0 0] 10 [0 0] }))))
+  
--- src/bowling_kata/core.clj
+++ src/bowling_kata/core.clj
@@ -1,4 +1,21 @@
 (ns bowling-kata.core)
 
+(defn spare? [[first-throw second-throw]]
+  (if (= 10 (+ first-throw second-throw))
+    true
+    false))
+
+(defn frame-bonus [frame next-frame]
+  (cond
+    (spare? frame) (first next-frame) 
+    :else 0))
+
+(defn calculate-bonus [frames]
+  (loop [frame-index 1 total-bonus 0]
+    (let [frame (get frames frame-index)]
+      (if (= frame-index 10) 
+      total-bonus
+      (recur (inc frame-index) (+ total-bonus (frame-bonus frame (get frames (inc frame-index)))))))))
+
 (defn score-game [frames]
-	(apply + (flatten (vals frames))))
+  (+ (apply + (flatten (vals frames))) (calculate-bonus frames)))
